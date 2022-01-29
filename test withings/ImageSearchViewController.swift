a9376949abe214830168f3507c62cbd991e2ffd8
Pierre Gabory <piergabory@icloud.com>
2022-01-29 15:08:55 +0100

    search and select

--- test withings/ImageSearchViewController.swift
+++ test withings/ImageSearchViewController.swift
@@ -19,15 +19,31 @@ class ImageSearchViewController: UIViewController {
     var results: [Response.Hit] = [] { didSet { collectionView.reloadData() } }
     
     @IBOutlet private var collectionView: UICollectionView!
+    @IBOutlet private var nextButton: UIBarButtonItem!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         collectionView.reloadData()
+        collectionView.allowsMultipleSelection = true
+        fetchImagesUrls()
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard
+            let slideshow = segue.destination as? SlideshowViewController,
+            let items = collectionView.indexPathsForSelectedItems?.map({ results[$0.item].previewURL })
+        else {
+            return
+        }
+        slideshow.urls = items
     }
     
     @IBAction private func fetchImagesUrls(search: UITextField? = nil) {
-        let request = "https://pixabay.com/api/?key=18021445-326cf5bcd3658777a9d22df6f&q=\(search?.text ?? "")"
-        URLSession.shared.dataTask(with: URL(string: request)!) { data, _, error in
+        updateNextButton()
+        guard
+            let request = URL(string: "https://pixabay.com/api/?key=18021445-326cf5bcd3658777a9d22df6f&q=\(search?.text ?? "")")
+        else { return }
+        URLSession.shared.dataTask(with: request) { data, _, error in
             do {
                 if let error = error {
                     throw error
@@ -45,6 +61,11 @@ class ImageSearchViewController: UIViewController {
             }
         }.resume()
     }
+    
+    private func updateNextButton() {
+        let numberOfSelectedItems = collectionView.indexPathsForSelectedItems?.count ?? 0
+        nextButton.isEnabled = numberOfSelectedItems >= 2
+    }
 }
     
 extension ImageSearchViewController: UICollectionViewDataSource {
@@ -56,6 +77,19 @@ extension ImageSearchViewController: UICollectionViewDataSource {
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Image", for: indexPath) as! ImageSearchCell
         cell.setImage(url: results[indexPath.row].previewURL)
+        cell.contentView.backgroundColor = .white
         return cell
+    }
+}
+
+extension ImageSearchViewController: UICollectionViewDelegate {
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        updateNextButton()
+        collectionView.cellForItem(at: indexPath)?.contentView.backgroundColor = .blue
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didDeselectItemAt indexPath: IndexPath) {
+        updateNextButton()
+        collectionView.cellForItem(at: indexPath)?.contentView.backgroundColor = .white
     }
 }
---
+++ test withings/SlideshowViewController.swift
@@ -0,0 +1,13 @@
+//
+//  SlideshowViewController.swift
+//  test withings
+//
+//  Created by Pierre Gabory on 29/01/2022.
+//
+
+import UIKit
+
+final class SlideshowViewController: UIViewController {
+
+    var urls: [URL] = []
+}
